22c9933f6e81c1c6c60880638f469031b4563bf7
Rubanraj <rubanraj54@gmail.com>
2018-12-27 16:26:58 +0100

    rendering basic input components finished

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
     <div id="app">
         <div class="container">
-            <vue-bootstrap4-form-generator>
-
+            <vue-bootstrap4-form-generator :model="model" :schema="schema">
             </vue-bootstrap4-form-generator>
         </div>
     </div>
@@ -13,6 +12,59 @@
 
     export default {
         name: 'App',
+        data: function() {
+            return {
+                model: {
+                    "id": 1,
+                    "name": {
+                        "first_name": "Vladimir",
+                        "last_name": "Nitzsche"
+                    },
+                    "address": {
+                        "country": "Mayotte"
+                    },
+                    "email": "franecki.anastasia@gmail.com",
+                },
+                schema: {
+                    type: "Object",
+                    elements: [
+                        {
+                            label: "ID",
+                            name: "id",
+                            element_type: "input",
+                            type: "number",
+                            placeholder: "Enter Id"
+                        },
+                        {
+                            label: "Email",
+                            name: "email",
+                            element_type: "input",
+                            type: "email",
+                            placeholder: "Enter Email"
+                        },
+                        {
+                            label: "Name",
+                            name: "name",
+                            type: "Object",
+                            elements: [
+                                {
+                                    label: "First Name",
+                                    name: "first_name",
+                                    element_type: "input",
+                                    input_type: "text"
+                                },
+                                {
+                                    label: "Last Name",
+                                    name: "last_name",
+                                    element_type: "input",
+                                    input_type: "text"
+                                },
+                            ]
+                        }
+                    ]
+                }
+            }
+        },
         components: {
             VueBootstrap4FormGenerator
         }
--- src/components/VueBootstrap4FormGenerator.vue
+++ src/components/VueBootstrap4FormGenerator.vue
@@ -1,16 +1,59 @@
 <template>
     <div>
-
+        <template v-if="schema.type === 'Object'">
+            <!-- <span>{{schema.name}}</span> -->
+            <div v-for="(element, key, index) in schema.elements" :key="index">
+                <!-- <span>{{element.label}}</span> -->
+                <template v-if="element.element_type === 'input'">
+                      <div class="form-group">
+                        <label>{{element.label}}</label>
+                        <input :type="element.type" class="form-control" v-model='model[element.name]' @keyup.stop="updateModel($event,element.name)" :placeholder="element.placeholder">
+                        <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+                    </div>
+                </template>
+                <template v-if="element.type === 'Object'">
+                    <vue-bootstrap4-form-generator :model="model[element.name]" :schema="element">
+                    </vue-bootstrap4-form-generator>
+                </template>
+            </div>
+        </template>
     </div>
 </template>
 
 <script>
+
+import VueBootstrap4FormGenerator from './VueBootstrap4FormGenerator'
+
 export default {
     name: "VueBootstrap4FormGenerator",
-    data: function() {
-        return {
-            msg: 2
+    props: {
+        model: {
+            type: Object,
+            default: function () {
+                return {}
+            }
+        },
+        schema: {
+            type: Object,
+            default: function () {
+                return {}
+            }
         }
     },
+    data: function() {
+        return {
+        }
+    },
+    mounted() {
+
+    },
+    methods: {
+        updateModel(event,name) {
+            console.log(event);
+        }
+    },
+    components: {
+        VueBootstrap4FormGenerator
+    }
 }
 </script>
